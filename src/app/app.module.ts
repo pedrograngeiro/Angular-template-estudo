e6f4328ca5f434f889619f63d085c30c165852bb
Pedro Grangeiro <46334439+pedrograngeiro@users.noreply.github.com>
2023-03-27 22:40:04 -0300

    string interpolation estudo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { OlaMundoComponent } from './ola-mundo/ola-mundo.component';
 import { ContadorComponent } from './contador/contador.component';
+import { StringInterpolationComponent } from './string-interpolation/string-interpolation.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     OlaMundoComponent,
-    ContadorComponent
+    ContadorComponent,
+    StringInterpolationComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/string-interpolation/string-interpolation.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-string-interpolation',
+  templateUrl: './string-interpolation.component.html',
+  styleUrls: ['./string-interpolation.component.css'],
+})
+export class StringInterpolationComponent {
+  public imgURL = 'https://devmedia.com.br/logo.png';
+  public url = 'https://devmedia.com.br';
+}
